489b7f9f746a5f340ec5f7147268e58852ed3a8b
gutodidonato <rm99433@fiap.com.br>
2023-12-05 16:42:41 -0300

    teste

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 function selectItem(itemNumber) {
+    const corpoMenu = document.querySelector('.corpo_menu');
     const items = document.querySelectorAll('.item');
-    items.forEach(item => item.classList.remove('selected'));
+
+    // Adiciona a classe "animating" ao corpoMenu quando uma animação inicia
+    corpoMenu.classList.add('animating');
+
+    items.forEach(item => {
+        item.classList.remove('selected', 'active');
+    });
 
     const selectedItem = document.querySelector(`.item:nth-child(${itemNumber})`);
-    selectedItem.classList.add('selected');
+    if (selectedItem) {
+        selectedItem.classList.add('selected', 'active');
+    }
 }
+
+document.body.addEventListener('click', function (event) {
+    const corpoMenu = document.querySelector('.corpo_menu');
+    const items = document.querySelectorAll('.item');
+    const isClickedOnItem = Array.from(items).some(item => item.contains(event.target));
+
+    if (!isClickedOnItem) {
+        // Remove a classe "animating" após o clique fora de um item
+        corpoMenu.classList.remove('animating');
+
+        items.forEach(item => item.classList.remove('active'));
+    }
+});
